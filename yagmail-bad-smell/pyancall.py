2e2df07fcbbe7144576f779be8c30033492d3eae
z0gSh1u <136371391@qq.com>
2021-11-27 20:45:14 +0800

    bad smell

--- yagmail-bad-smell/pyancall.py
+++ yagmail-bad-smell/pyancall.py
@@ -1,6 +1,6 @@
 import pyan
 
-cg = pyan.create_callgraph(r'F:/sa-analysis/yagmail-bad-smell/yagmail/**/*.py', format='dot', draw_defines=False, draw_uses=True)
+cg = pyan.create_callgraph(r'F:/sa-analysis/yagmail-bad-smell/yagmail/**/*.py', format='dot', draw_defines=False, draw_uses=True, namespace='yagmail.sender')
 
 with open('dot.dot', 'w') as f:
     f.write(cg)
